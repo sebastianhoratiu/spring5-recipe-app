eb2239ae08108f6f0c141740c18677444561e7a3
Suciu, Sebastian <Sebastian.Suciu@garmin.com>
2024-12-10 13:41:03 +0200

    Lesson-176 Mockito Argument Capture. This  was interesting but, I don't get why we use it because: 1. We know what getRecipes will return, since we're told it what to return in the when() statement; 2. Since we are testing getIndexPage here, why do we also want to test what recipeService does? Shouldn't we test that in the recipeService dedicated test?

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,12 +1,18 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.*;
 import guru.springframework.services.*;
 import junit.framework.TestCase;
 import org.junit.*;
 import org.mockito.*;
 import org.springframework.ui.*;
 
+import java.util.*;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class IndexControllerTest {
 
@@ -27,9 +33,27 @@ public class IndexControllerTest {
 
     @Test
     public void testGetIndexPage() {
+        //given
+        Set<Recipe> recipes = new HashSet<Recipe>();
+
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String viewName = indexController.getIndexPage(model);
+
+        //then
         assertEquals("index", viewName);
-        Mockito.verify(recipeService, Mockito.times(1)).getRecipes();
-        Mockito.verify(model, Mockito.times(1)).addAttribute(Mockito.anyString(), Mockito.any());
+        verify(recipeService, Mockito.times(1)).getRecipes();
+        verify(model, Mockito.times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        assertEquals(2, argumentCaptor.getValue().size());
+        assertEquals(recipes, argumentCaptor.getValue());
     }
 }
